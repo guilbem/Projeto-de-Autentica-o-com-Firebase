b0d693c3839b9345886e63ef517525321fb925d4
Guilherme Guimarães <guilherme57214@estudante.ifb.edu.br>
2025-10-27 11:53:40 -0300

    adcionando storage.js

---
+++ storage.js
@@ -0,0 +1,29 @@
+var uploader = document.getElementById('uploader');
+var fileButton = document.getElementById('fileButton');
+
+//ouvir o evento change
+fileButton.addEventListener('change', function (e){
+
+    //obter arquivo
+    var file = e.target.files[0];
+
+    //referenciar o storage
+    var storageRef = firebase.storage().ref('arquivos/' + file.name);
+
+    //enviar arquivos
+    var task = storageRef.put(file);
+
+    //atualizar o progress bar
+    task.on('state_changed',
+        function progress(snapshot) {
+            var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            uploader.value = percentage;
+        },
+        function error(err){
+            console.log(err);
+        },
+        function complete(){
+            alert('Envio completo!')
+        }
+    )
+});
